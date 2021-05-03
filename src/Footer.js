ad3073f517394d010a9910550fd0d83f2a29e925
NakibDiu <nakib15-3358@diu.edu.bd>
2021-05-04 04:59:09 +0600

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+
+const Footer = () => {
+    return (
+        <div className = "footer">
+            <h3 className = "footText">Made with ❤ By Nakib</h3>
+        </div>
+    ) 
+}
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const Header = () => {
+    return (
+        <div>
+            <div className = "nav">
+                <h1 className = "greetings">Welcome</h1>
+            </div>
+        </div>
+    ) 
+}
+export default Header;
